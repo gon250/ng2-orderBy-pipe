9fd3b4b7e57c2c5d9afd85f9d42bbaaad7753d31
Gonzalo <zalo250@gmail.com>
2016-11-05 19:29:14 +0000

    Implementing order by date

--- pipes/order-by-date.pipe.ts
+++ pipes/order-by-date.pipe.ts
@@ -1,10 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { isArray } from '../utils/utils';
+import { isNull } from '../utils/utils';
 @Pipe({
     name: 'orderByDate'
 })
 export class OrderByDate implements PipeTransform {
-    transform(values: any[], filter: any): any {
-        
+    transform(values: number[], filter: any): any {
+        if(isNull(values)) {
+            return null;
+        }
+        values.sort(function(a,b) { 
+            return new Date(a).getTime() - new Date(b).getTime() 
+        });
     }
  }
