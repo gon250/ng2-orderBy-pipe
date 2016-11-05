7ae5d915e4a8b979f5c5dcec8f17a4091db3b5bc
Gonzalo <zalo250@gmail.com>
2016-11-05 19:26:18 +0000

    Implementing order by number

--- pipes/order-by-number.pipe.ts
+++ pipes/order-by-number.pipe.ts
@@ -1,11 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { isArray } from '../utils/utils';
+import { isNull } from '../utils/utils';
 @Pipe({
     name: 'orderByNumber'
 })
 export class OrderByNumber implements PipeTransform {
-    transform(values: any[], filter: any): any {
-        
+    transform(values: number[], filter: any): any {
+        if(isNull(values)) {
+            return null;
+        }
+        values.sort((a: number, b: number) => {
+            if (a < b) {
+                return -1;
+            } else if (a > b) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+    return values;
     }
  }
  
