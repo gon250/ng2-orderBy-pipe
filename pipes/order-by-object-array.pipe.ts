3d34b543160c3f3eb490a753629db7ecd780ade1
Gonzalo <zalo250@gmail.com>
2016-11-06 17:56:06 +0000

    Adding function OrderByArray

--- pipes/order-by-object-array.pipe.ts
+++ pipes/order-by-object-array.pipe.ts
@@ -4,22 +4,28 @@ import { isNull, isNumber, isString } from '../utils/utils';
     name: 'orderByArrayObject'
 })
 export class orderByArrayObject implements PipeTransform {
-    transform(values: any[], filter: any, orderType: any): any {
+    transform(values: any[], filter: any, orderType: string): any {
         if(isNull(values)) {return null; }
-
         //TODO: Manage ifs depends on  orderType.
-        values.sort((a, b) => {
+        return this.OrderByArray(values, orderType).map(item => item.orderType);
+    }
+
+    OrderByArray(values: any[], orderType: any) { //TODO: implement filter option.
+        return values.sort((a, b) => {
             if (a[orderType] < b[orderType]) {
                 return -1;
             }
-
             if (a[orderType] > b[orderType]) {
                 return 1;
             }
-
             return 0
         });
-
-        return values;
     }
+    
+    //TODO: future update for typescript 2.1 
+    /*
+    function OrderByArray<T, K keyof T>(values: T[], orderType: K) {
+        ...
+    }
+    */ç
  }
