0ff1d473f96b95cad7bc87bf2476a125d4df8249
Gonzalo <zalo250@gmail.com>
2016-11-06 16:04:50 +0000

    working on order by array

--- pipes/order-by-object-array.pipe.ts
+++ pipes/order-by-object-array.pipe.ts
@@ -7,7 +7,18 @@ export class orderByArrayObject implements PipeTransform {
     transform(values: any[], filter: any, orderType: any): any {
         if(isNull(values)) {return null; }
 
-        
+        //TODO: Manage ifs depends on  orderType.
+        values.sort((a, b) => {
+            if (a[orderType] < b[orderType]) {
+                return -1;
+            }
+
+            if (a[orderType] > b[orderType]) {
+                return 1;
+            }
+
+            return 0
+        });
 
         return values;
     }
