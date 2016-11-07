6f15f385aad341bd3f3b68234800f8229ded43f4
Gonzalo <zalo250@gmail.com>
2016-11-07 19:16:27 +0000

    new way to sort strings descending

--- pipes/order-by-string.pipe.ts
+++ pipes/order-by-string.pipe.ts
@@ -1,17 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { isNull } from '../utils/utils';
-
 @Pipe({
     name: 'orderByString'
 })
 export class OrderByString implements PipeTransform {
     transform(values: string[], filter?: any): any {
-        if(isNull(values)) {
-            return null;
-        }
+        if(isNull(values)) { return null; }
         if (filter === 'descending'){
-            values.sort();
-            values.reverse();
+            values.sort((a,b) => 0 - (a > b ? 1 : -1));
         }else{
             values.sort();
         }
