05cec1fc5a2f31a2a59c8b7e5cfb6a1362c82d7f
Gonzalo <zalo250@gmail.com>
2016-11-07 18:47:44 +0000

    Adding a TODO in orderByDate

--- pipes/order-by-date.pipe.ts
+++ pipes/order-by-date.pipe.ts
@@ -1,19 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { isNull } from '../utils/utils';
+import { isNull, isDate } from '../utils/utils';
 @Pipe({
     name: 'orderByDate'
 })
 export class OrderByDate implements PipeTransform {
     transform(values: any[], filter?: string): any {
-        if(isNull(values)) {
-            return null;
-        }
+        if(isNull(values)) { return null; }
+
+        //TODO: validate date values.
         if (filter === 'descending'){
-            values.sort(function(a,b) { 
+            values.sort(function(a, b) { 
                 return new Date(a).getTime() - new Date(b).getTime() 
             });
         }else{
-            values.sort(function(a,b) { 
+            values.sort(function(a, b) { 
                 return new Date(b).getTime() - new Date(a).getTime() 
             });
         }
