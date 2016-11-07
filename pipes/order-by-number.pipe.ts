0ff9f750a03ee6c7f6055707f65c7e2e3b71c466
Gonzalo <zalo250@gmail.com>
2016-11-07 18:48:56 +0000

    making sure items are nunmbers in orderByNumber

--- pipes/order-by-number.pipe.ts
+++ pipes/order-by-number.pipe.ts
@@ -5,13 +5,11 @@ import { isNull } from '../utils/utils';
 })
 export class OrderByNumber implements PipeTransform {
     transform(values: number[], filter?: string): any {
-        if(isNull(values)) {
-            return null;
-        }
+        if(isNull(values)) {  return null; }
         if (filter === 'descending'){
-            values.sort(function(a, b){return b-a});
+            values.sort(function(a: number, b: number){return b-a});
         }else{
-            values.sort(function(a, b){return a-b});
+            values.sort(function(a:number, b:number){return a-b});
         }
     return values;
     }
