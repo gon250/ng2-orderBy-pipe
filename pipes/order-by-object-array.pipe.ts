266942888ef5412c7dc8ab66d385bb77e7f38e35
adblancod <adblancod@gmail.com>
2016-11-08 18:47:54 +0000

    Added Descending filter to orderByArrayObject pipe

--- pipes/order-by-object-array.pipe.ts
+++ pipes/order-by-object-array.pipe.ts
@@ -5,26 +5,25 @@ import { isNull, isNumber, isString } from '../utils/utils';
 })
 export class orderByArrayObject implements PipeTransform {
     transform(values: any[], orderType: string, filter: string): any[] {
-        if(isNull(values)) {return null; }
-        return this.OrderByArray(values, orderType);
+        if (isNull(values)) { return null; }
+        return this.OrderByArray(values, orderType, filter === 'descending');
     }
 
-    OrderByArray(values: any[], orderType: string) : any[] { //TODO: implement filter option.
+    OrderByArray(values: any[], orderType: string, descending: boolean): any[] { //TODO: implement filter option.
         return values.sort((a, b) => {
             if (a[orderType] < b[orderType]) {
-                return -1;
-            }
-            if (a[orderType] > b[orderType]) {
-                return 1;
+                return descending ? 1 : -1;
+            } else if (a[orderType] > b[orderType]) {
+                return descending ? -1 : 1;
             }
             return 0
         });
     }
-    
+
     //TODO: future update for typescript 2.1 
     /*
     function OrderByArray<T, K keyof T>(values: T[], orderType: K) {
         ...
     }
     */
- }
+}
